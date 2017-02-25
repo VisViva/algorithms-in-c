d79b3541e5a8d753fdcc1f0b5d6775458e649c83
Vis Viva <a.vis.viva@gmail.com>
2017-02-25 21:01:00 +0300

    Npm based build system

--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@ main() {
   int x, y;
 
   fraction frac = {
-  	.numerator = 1,
-  	.denominator = 1
+   .numerator = 1,
+   .denominator = 1
   };
 
   while (scanf("%d %d", &x, &y) != EOF) {
     frac.numerator = x;
-  	frac.denominator = y;
-  	to_lowest_terms(&frac);
-    if (x > 0 && y > 0) printf("%d/%d => %d/%d\n", x, y, frac.numerator, frac.denominator);
+   frac.denominator = y;
+   to_lowest_terms(&frac);
+    if (x > 0 && y > 0) printf("%d/%d => %d/%d\n", x, y, frac.numerator,
+    frac.denominator);
   }
 }
