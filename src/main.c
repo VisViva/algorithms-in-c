2c75207c1e8e83a7c65267d673197b7ea4c81cf7
Vis Viva <a.vis.viva@gmail.com>
2017-02-25 21:04:31 +0300

    Generating makefile via watcher script

--- src/main.c
+++ src/main.c
@@ -5,16 +5,13 @@
 main() {
   int x, y;
 
-  fraction frac = {
-   .numerator = 1,
-   .denominator = 1
-  };
+  fraction frac = {.numerator = 1, .denominator = 1};
 
-  while (scanf("%d %d", &x, &y) != EOF) {
+  if (scanf("%d %d", &x, &y) != EOF) {
     frac.numerator = x;
-   frac.denominator = y;
-   to_lowest_terms(&frac);
-    if (x > 0 && y > 0) printf("%d/%d => %d/%d\n", x, y, frac.numerator,
-    frac.denominator);
+    frac.denominator = y;
+    to_lowest_terms(&frac);
+    if (x > 0 && y > 0)
+      printf("%d/%d => %d/%d\n", x, y, frac.numerator, frac.denominator);
   }
 }
